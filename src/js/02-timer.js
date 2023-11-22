75e9afaf3dae30cc0a6f5167379a9b3df6cc44b0
Dmytro1205 <134944225+Dmytro1205@users.noreply.github.com>
2023-11-22 21:02:38 +0200

    21

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,40 +1,37 @@
-// import Notiflix from 'notiflix';
 // Описаний в документації
-import flatpickr from "flatpickr";
+import flatpickr from 'flatpickr';
 // Додатковий імпорт стилів
-import "flatpickr/dist/flatpickr.min.css";
+import 'flatpickr/dist/flatpickr.min.css';
 const startBtn = document.querySelector('[data-start]');
 const daysEl = document.querySelector('[data-days]');
 const hoursEl = document.querySelector('[data-hours]');
 const minutesEl = document.querySelector('[data-minutes]');
 const secondsEl = document.querySelector('[data-seconds]');
-const input = document.querySelector("#datetime-picker")
+const input = document.querySelector('#datetime-picker');
 
 let countdownInterval;
 
 startBtn.disabled = true;
 const options = {
-        enableTime: true,
-        time_24hr: true,
-        defaultDate: new Date(),
-        minuteIncrement: 1,
-        onClose(selectedDates) {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
     const selectedDate = selectedDates[0];
     if (selectedDate < new Date()) {
       alert('Please choose a date in the future');
       return;
     }
     startBtn.disabled = false;
-    
   },
-}
-const datePicker = flatpickr("#datetime-picker",options);
+};
+const datePicker = flatpickr('#datetime-picker', options);
 startBtn.addEventListener('click', () => {
   startCountdown(datePicker.selectedDates[0]);
   startBtn.disabled = true;
-  input.disabled= true
+  input.disabled = true;
 });
-
 
 function startCountdown(endTime) {
   countdownInterval = setInterval(() => {
@@ -49,23 +46,23 @@ function startCountdown(endTime) {
 }
 
 function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
-  
-    // Remaining days
-    const days = Math.floor(ms / day);
-    // Remaining hours
-    const hours = Math.floor((ms % day) / hour);
-    // Remaining minutes
-    const minutes = Math.floor(((ms % day) % hour) / minute);
-    // Remaining seconds
-    const seconds = Math.floor((((ms % day) % hour) % minute) / second);
-  
-    return { days, hours, minutes, seconds };
-  }
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
 
 function addLeadingZero(value) {
   return value.toString().padStart(2, '0');
