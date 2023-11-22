594001fd84d83faabb2b71c32287c1ccb4bfe2af
Dmytro1205 <134944225+Dmytro1205@users.noreply.github.com>
2023-11-22 20:46:41 +0200

    21

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,27 @@
+const body = document.body;
+const stop = document.querySelector('[data-stop]');
+const start = document.querySelector('[data-start]');
+let timerId = null
+
+stop.disabled = true;
+
+start.addEventListener("click", changeColor) 
+
+stop.addEventListener('click', stopChangeColor);
+
+function changeColor(event) {
+    timerId = setInterval(() => {
+    body.style.backgroundColor= getRandomHexColor()
+        }, 1000
+     )
+     start.disabled = true;
+     stop.disabled = false;
+
+}
+
+function stopChangeColor(event) {
+        clearInterval(timerId);   
+        start.disabled = false;
+        stop.disabled = true;
+
+}
