18f027d00478ab859e90b0474f1a950dd6fdbc8e
Dmytro1205 <134944225+Dmytro1205@users.noreply.github.com>
2023-11-22 20:56:01 +0200

    23

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,3 +1,6 @@
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
 const body = document.body;
 const stop = document.querySelector('[data-stop]');
 const start = document.querySelector('[data-start]');
