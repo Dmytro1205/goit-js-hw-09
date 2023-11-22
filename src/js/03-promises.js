532476c3512375c72427d897ea07fa6008527568
Dmytro1205 <134944225+Dmytro1205@users.noreply.github.com>
2023-11-22 20:40:27 +0200

    212

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,33 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onFormSubmit);
+function onFormSubmit(event) {
+  event.preventDefault();
+  const { delay, step, amount } = event.currentTarget;
+  let currentDelay = Number(delay.value);
+  for (let i = 1; i < amount.value; i += 1) {
+    createPromise(i, currentDelay)
+      .then(({ position, delay }) => {
+        console.log(`Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`Rejected promise ${position} in ${delay}ms`);
+      });
+    currentDelay += Number(step.value);
   }
+  event.target.reset();
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    //setTimeout для першої затримки delay
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
